6a85f060ccc097375303b34bbfd1a7deedf7785c
mitech <kagudaprince@gmail.com>
2017-08-17 06:03:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 curr_dir="$(pwd)"
 place_holder="CURR_DIR"
-sed "s|$place_holder|$curr_dir|g" $curr_dir/docker-compose.yml
+sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/docker-compose.yml
 chmod +x $curr_dir/obsidian/entrypoint.sh
 chmod +x $curr_dir/obsidian/startup.sh
 chmod +x $curr_dir/obsidian/set-env.sh
