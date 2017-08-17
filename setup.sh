841248582ab41699886e0e8b39c15baee5ebb742
Prince Kaguda <princek@discovery.co.za>
2017-08-17 10:46:40 +0200

    add compose scripts for db and platform services

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+echo "Starting vdx setup"
 curr_dir="$(pwd)"
 place_holder="CURR_DIR"
 sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/docker-compose.yml
-sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/db-docker-compose.yml
+sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/db-compose.yml
+sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/platform-services-compose.yml
 chmod +x $curr_dir/setupfiles/obsidian/entrypoint.sh
 chmod +x $curr_dir/setupfiles/obsidian/startup.sh
 chmod +x $curr_dir/setupfiles/obsidian/set-env.sh
@@ -11,3 +12,4 @@ mkdir $curr_dir/volumes
 mkdir $curr_dir/volumes/sftp
 mkdir $curr_dir/volumes/portainer
 mkdir $curr_dir/volumes/oracle
+echo "Done with setup"
