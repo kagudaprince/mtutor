82c56210f6ca5cda243b6e75def0e059eb097f13
Prince Kaguda <princek@discovery.co.za>
2017-08-17 10:39:24 +0200

    add compose scripts for db and platform services

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 curr_dir="$(pwd)"
 place_holder="CURR_DIR"
 sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/docker-compose.yml
+sed -i -E "s|$place_holder|$curr_dir|g" $curr_dir/db-docker-compose.yml
 chmod +x $curr_dir/setupfiles/obsidian/entrypoint.sh
 chmod +x $curr_dir/setupfiles/obsidian/startup.sh
 chmod +x $curr_dir/setupfiles/obsidian/set-env.sh
