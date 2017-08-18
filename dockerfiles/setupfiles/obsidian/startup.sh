6bc8b3872e9e0c3b0141ee02ca5af330a52e7316
mitech <kagudaprince@gmail.com>
2017-08-18 08:43:39 +0200

    Update startup.sh

--- dockerfiles/setupfiles/obsidian/startup.sh
+++ dockerfiles/setupfiles/obsidian/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 exec 
-   sed -i -e "s/DB_USER/`echo $DB_USER`/g" /opt/obsidian/4.3.0/obsidian.properties \
-   && sed -i -e "s/DB_PASS/`echo $DB_PASS`/g" /opt/obsidian/4.3.0/obsidian.properties \
-   && sed -i -e "s/DB_URL/`echo $DB_URL`/g" /opt/obsidian/4.3.0/obsidian.properties \
+   sed -i -e "s|DB_USER|`echo $DB_USER`|g" /opt/obsidian/4.3.0/obsidian.properties \
+   && sed -i -e "s|DB_PASS|`echo $DB_PASS`|g" /opt/obsidian/4.3.0/obsidian.properties \
+   && sed -i -e "s|DB_URL|`echo $DB_URL`|g" /opt/obsidian/4.3.0/obsidian.properties \
    && "/usr/bin/java" \
   -jar /opt/obsidian/4.3.0/jetty/start.jar \
   -Duser.timezone=$TIMEZONE \
